c1d38fd8d2a60efd38e3fc6fa11e3a0ade32857d
William Go <gilberthoop@Williams-MacBook-Pro.local>
2021-10-11 14:32:32 -0700

    Deprecate Typescript in app

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar>
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon :icon="triangle" />
           <ion-label>Tab 1</ion-label>
@@ -22,7 +22,7 @@
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 
