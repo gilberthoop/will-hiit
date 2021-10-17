3de854ead98936ec900dba673495d29c6f53e981
William Go <gilberthoop@Williams-MacBook-Pro.local>
2021-10-16 17:12:19 -0700

    Add all HIIT workout components

--- src/components/hiit-workouts/InAndOut.vue
+++ src/components/hiit-workouts/InAndOut.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1>High Knees</h1>
+        <h1>In and Outs</h1>
         <div
             v-if="workoutTimer !== workoutDuration"
         >
@@ -18,7 +18,7 @@
 
 <script>
 export default {
-    name: 'HighKnee',
+    name: 'InAndOut',
 
     props: {
         workoutDuration: Number,
